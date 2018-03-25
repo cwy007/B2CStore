9e1e3f3f20118dd83f0944444995ad400ee55b3f
cwy007 <chanweiyan007@gmail.com>
2018-03-26 00:09:20 +0800

    Implement signup, login & logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
+  delete '/logout' => 'sessions#destroy', as: :logout
 end
